f936f555a820cd273c25f36bc56d2f282c390144
Sharmr <s98raghav@gmail.com>
2021-11-22 13:27:19 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { GameWindow } from './components/GameWindow';
+
+export const App = () => {
+    return (<>
+                <h1>Something</h1>
+                <GameWindow />
+            </>)
+};
---
+++ src/components/GameWindow.js
@@ -0,0 +1,5 @@
+export const GameWindow = () => {
+    return (<div>
+                <h1>Game Window goes here</h1>
+            </div>)
+};
